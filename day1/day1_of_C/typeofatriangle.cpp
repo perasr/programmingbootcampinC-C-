22959dc0c291ae3a9d58c24e3a6773774b749ce1
SRIGANESH PERA <p.sriganesh@yahoo.com>
2019-04-26 22:31:55 -0500

    Add files via upload

--- day1/day1_of_C/typeofatriangle.cpp
+++ day1/day1_of_C/typeofatriangle.cpp
@@ -7,11 +7,14 @@
    printf("Enter the three sides of a triangle: ");
    scanf("%d%d%d",&a,&b,&c);
 /*display the type of triangle*/
+
 if(a+b>c || b+c>a || c+b>a)
 {
-  if(a == b & b==c)
+ if (a==0 || b==0 | c==0)
+ printf("The sides(%d,%d,%d) doesnot form a triangle",a,b,c);
+  else if(a == b & b==c)
   printf("the sides (%d, %d ,%d) forms an equilateral triangle",a,b,c);
-  else if(a == b || b==c || c == a)
+  else if((a == b || b==c || c == a) && (a!=0 || b!=0 || c!=0))
   printf("the sides (%d, %d, %d) forms an Isoceles trainge",a,b,c);
   else
   printf("the sides (%d,%d,%d) forms an scalane triangle",a,b,c);
@@ -20,3 +23,5 @@ else
 printf("The sides(%d,%d,%d) doesnot form a traingle at all",a,b,c);
   return 0;
  }
+ 
+ 
